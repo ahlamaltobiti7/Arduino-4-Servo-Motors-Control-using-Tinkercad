2d24a03c81bec479b1d320560d9d580ac3869919
Ahlam Abdulgani Althobaiti <ahlamm3mm@gmail.com>
2026-07-15 02:30:29 +0300

    Add files via upload

---
+++ servo_motors_control1 (1).ino
@@ -0,0 +1,40 @@
+// C++ code
+//
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+void setup() {
+ 
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+
+  for (int pos = 0; pos <= 180; pos += 1) {
+    servo1.write(pos);
+    servo2.write(pos);
+    servo3.write(pos);
+    servo4.write(pos);
+    delay(5);
+  }
+
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    servo1.write(pos);
+    servo2.write(pos);
+    servo3.write(pos);
+    servo4.write(pos);
+    delay(5); 
+  }
+
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+}
+
+void loop() {
+}
